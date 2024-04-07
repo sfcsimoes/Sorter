f49604b9abed118afe862d09c46b7ffbce5f6ca4
Gabriel M. Franzin <bielmarfran@gmail.com>
2024-04-07 15:55:45 +0100

    Update to Schema

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -14,6 +14,26 @@ export const productsRelations = relations(products, ({ many }) => ({
 	productsInShipmentOrders: many(productsInShipmentOrders),
 }));
 
+export const warehouses = sqliteTable('warehouses', {
+	id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+	name: text('name'),
+	adress: text('adress'),
+	createdAt: text("createdAt").default(sql`(CURRENT_TIMESTAMP)`),
+	updatedAt: text("updatedAt").default(sql`(CURRENT_TIMESTAMP)`),
+});
+
+export const warehousesRelations = relations(warehouses, ({ many }) => ({
+	shipmentOrders: many(shipmentOrders),
+}));
+
+export const sorterEntryPoints = sqliteTable('sorterEntryPoints', {
+	id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+	name: text('name'),
+	active: integer('active', { mode: 'boolean' }),
+	createdAt: text("createdAt").default(sql`(CURRENT_TIMESTAMP)`),
+	updatedAt: text("updatedAt").default(sql`(CURRENT_TIMESTAMP)`),
+});
+
 export const stores = sqliteTable('stores', {
 	id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
 	name: text('name'),
@@ -27,8 +47,8 @@ export const storesRelations = relations(stores, ({ many }) => ({
 
 export const shipmentOrders = sqliteTable('shipmentOrders', {
 	id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+	warehouse: integer('warehouseId').references(() => warehouses.id),
 	store: integer('storeId').references(() => stores.id),
-	// product: integer('productId').references(() => products.id),
 	status: text('status', { enum: ['Concluida', 'Cancelada', 'Pendente'] }),
 	createdAt: text("createdAt").default(sql`(CURRENT_TIMESTAMP)`),
 	updatedAt: text("updatedAt").default(sql`(CURRENT_TIMESTAMP)`),
@@ -59,36 +79,17 @@ export const productsInShipmentOrdersRelations = relations(productsInShipmentOrd
 	}),
 }));
 
-
 export type Product = typeof products.$inferSelect;
 export type NewProduct = typeof products.$inferInsert;
 
 export type Store = typeof stores.$inferSelect;
 export type NewStore = typeof stores.$inferInsert;
 
+export type Warehouse = typeof warehouses.$inferSelect;
+export type NewWarehouse= typeof warehouses.$inferInsert;
+
 export type ShipmentOrder = typeof shipmentOrders.$inferSelect;
 export type NewShipmentOrder = typeof shipmentOrders.$inferInsert;
 
 export type ProductsInShipmentOrders = typeof productsInShipmentOrders.$inferSelect;
 export type NewProductsInShipmentOrders = typeof productsInShipmentOrders.$inferInsert;
-
-// export type ShipmentOrderWithTransportationBox = typeof shipmentOrderWithTransportationBoxes.$inferSelect;
-// export type NewShipmentOrderWithTransportationBox = typeof shipmentOrderWithTransportationBoxes.$inferInsert;
-
-// export const users = sqliteTable('users', {
-// 	id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
-// 	name: text('name'),
-// });
-
-// export const ideas = sqliteTable('ideas', {
-// 	id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
-// 	text: text('text'),
-// 	status: text('status', { enum: ['approved', 'rejected', 'pending'] }),
-// 	creator: integer('creator_id').references(() => users.id),
-// });
-
-// export type User = typeof users.$inferSelect;
-// export type NewUser = typeof users.$inferInsert;
-
-// export type Idea = typeof ideas.$inferSelect;
-// export type NewIdea = typeof ideas.$inferInsert;
